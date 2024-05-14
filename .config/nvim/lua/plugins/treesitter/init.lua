aba6b24b687a4e497710eec584f20205eee7951c
angles-n-daemons <dillmann.brian@gmail.com>
2024-05-14 15:23:34 -0400

    add treesitter

--- .config/nvim/lua/plugins/treesitter/init.lua
+++ .config/nvim/lua/plugins/treesitter/init.lua
@@ -1,1 +1,82 @@
-return {}
+local function toggleContext()
+  local tsc = require('treesitter-context')
+  tsc.toggle()
+end
+
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    },
+
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+    },
+    config = function(_, _)
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup({
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              ["if"] = "@function.inner",
+              ["af"] = "@function.outer",
+              ["ic"] = "@class.inner",
+              ["ac"] = "@class.outer",
+              ["ib"] = "@block.inner",
+              ["ab"] = "@block.outer",
+            }
+          },
+          move = {
+            enable = true,
+            keymaps = {
+              ["if"] = "@function.inner",
+              ["af"] = "@function.outer",
+              ["ic"] = "@class.inner",
+              ["ac"] = "@class.outer",
+              ["ib"] = "@block.inner",
+              ["ab"] = "@block.outer",
+            }
+          },
+        },
+      })
+      -- what are the things that I want to try to configure
+      -- lazyvim seems to have a number of overrides:
+      --  - lots of languages ensure installed
+      --  - incremental selection
+      --  - text object configuration
+
+      -- What will I use treesitter for?
+      -- - syntax highlighting (ideally beyond the lsp?)
+      -- - text objects
+      -- - possible lsp interop
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'WinEnter',
+    enabled = true,
+    opts = { mode = 'cursor', max_lines = 10 },
+    keys = {
+      {
+        '<leader>ut',
+        toggleContext,
+        desc = 'Toggle Treesitter Context',
+      },
+    },
+    config = function()
+      require('treesitter-context').enable()
+    end
+  }
+}
