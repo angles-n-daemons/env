8ef892a4f721ef6ef434098b3da939aaa6a93064
angles-n-daemons <dillmann.brian@gmail.com>
2024-05-28 18:53:34 -0400

    add noice

---
+++ config/nvim/lua/godzilla/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+  config = function(_, opts)
+    require('noice').setup(opts)
+  end,
+}
