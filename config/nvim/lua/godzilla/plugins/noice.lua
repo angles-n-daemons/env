9f87fa3d90ea2cc0a626b5bb75f88bc2747b5784
angles-n-daemons <dillmann.brian@gmail.com>
2024-05-29 10:36:06 -0400

    some additional trouble configuration

--- config/nvim/lua/godzilla/plugins/noice.lua
+++ config/nvim/lua/godzilla/plugins/noice.lua
@@ -1,6 +1,9 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
+  keys = {
+    { '<leader>nl', '<cmd>Noice last<cr>', desc = '[N]otification, [L]ast' },
+  },
   dependencies = {
     'MunifTanjim/nui.nvim',
     'rcarriga/nvim-notify',
