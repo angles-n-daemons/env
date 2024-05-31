a2403e8eaa3b42f6740dc0d0e7bcb0ad3684c7e9
Brian Dillmann <dillmann.brian@gmail.com>
2024-05-30 22:28:25 -0400

    add keybindings back for messaging

--- config/nvim/lua/godzilla/plugins/noice.lua
+++ config/nvim/lua/godzilla/plugins/noice.lua
@@ -2,7 +2,7 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   keys = {
-    { '<leader>nl', '<C-n>', desc = 'nnnnnnnnn[N]oice!' },
+    { '<leader>nl', '<leader>nl', desc = '[N]oice, [L]ast' },
   },
   dependencies = {
     'MunifTanjim/nui.nvim',
