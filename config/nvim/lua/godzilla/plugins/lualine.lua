6a8a3d42921867c35b1d7e17f65dd9c047eb36a6
Brian Dillmann <dillmann.brian@gmail.com>
2024-06-09 00:44:42 -0400

    add lualine showmode messages

--- config/nvim/lua/godzilla/plugins/lualine.lua
+++ config/nvim/lua/godzilla/plugins/lualine.lua
@@ -2,7 +2,18 @@ return {
   'nvim-lualine/lualine.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup()
+  config = function(_, opts)
+    require('lualine').setup {
+      sections = {
+        lualine_y = {
+          'progress',
+          {
+            require('noice').api.statusline.mode.get,
+            cond = require('noice').api.statusline.mode.has,
+            color = 'WarningMsg',
+          },
+        },
+      },
+    }
   end,
 }
