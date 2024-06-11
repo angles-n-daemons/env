ac488f1917434f0b9b25165733318ee38f3f1c3d
angles-n-daemons <dillmann.brian@gmail.com>
2024-06-11 12:44:38 -0400

    fix catppuccin typo

--- config/nvim/lua/godzilla/plugins/colors.lua
+++ config/nvim/lua/godzilla/plugins/colors.lua
@@ -11,7 +11,7 @@ local colorConfigs = {
   default = { 'neanias/everforest-nvim', config = setColorScheme 'everforest' },
   python = { 'ellisonleao/gruvbox.nvim', config = setColorScheme 'gruvbox' },
   go = { 'zortax/three-firewatch', config = setColorScheme 'three-firewatch' },
-  typescript = { 'catppuccin', config = setColorScheme 'catppuccin' },
+  typescript = { 'catppuccin/nvim', name = 'catppuccin', config = setColorScheme 'catppuccin' },
   lua = { 'folke/tokyonight.nvim', config = setColorScheme 'tokyonight' },
   cpp = { 'miikanissi/modus-themes.nvim', config = setColorScheme 'modus' },
 }
