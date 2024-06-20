d127af5610393bfd2533f086a07666ce772ef2d5
angles-n-daemons <dillmann.brian@gmail.com>
2024-06-20 13:18:19 -0400

    go back to old quotes

--- config/nvim/lua/godzilla/plugins/fortune.lua
+++ config/nvim/lua/godzilla/plugins/fortune.lua
@@ -1,13 +1,7 @@
 return {
   'rubiin/fortune.nvim',
   opts = {
-    custom_quotes = {
-      short = {
-        {
-          "Life's but a walking shadow, a poor player\nThat struts and frets his hour upon the stage,\nAnd then is heard no more.\nIt is a tale Told by an idiot, full of sound and fury,\nSignifying nothing.",
-        },
-      },
-    },
+    content_type = 'mixed',
   },
   config = true,
 }
