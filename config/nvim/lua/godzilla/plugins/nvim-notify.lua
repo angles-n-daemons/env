145692e53a333498f621b224b6f8d6ebe43e2a5b
angles-n-daemons <brian.dillmann@cockroachlabs.com>
2024-09-06 16:11:46 -0400

    print out the path

--- config/nvim/lua/godzilla/plugins/nvim-notify.lua
+++ config/nvim/lua/godzilla/plugins/nvim-notify.lua
@@ -8,6 +8,13 @@ return {
       end,
       desc = '[N]otification [D]ismiss',
     },
+    {
+      '<leader>np',
+      function()
+        vim.notify(vim.api.nvim_buf_get_name(0))
+      end,
+      desc = '[N]otification current [P]ath',
+    },
   },
   config = function()
     vim.notify = require 'notify'
