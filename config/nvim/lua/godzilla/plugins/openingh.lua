90a9c60ebfb592dc780eea8747ccd869826cefb0
angles-n-daemons <brian.dillmann@cockroachlabs.com>
2024-09-06 16:08:03 -0400

    open in github

---
+++ config/nvim/lua/godzilla/plugins/openingh.lua
@@ -0,0 +1,11 @@
+return {
+  'almo7aya/openingh.nvim',
+  config = true,
+  keys = {
+    {
+      '<leader>gh',
+      '<cmd>OpenInGHFileLines<cr>',
+      desc = 'Open in [G]it[H]ub',
+    },
+  },
+}
